9fdc30138552ee2c1e9f00922652f864d6b0cb79
makavel1 <giorgi.gaprindashvili04@geolab.edu.ge>
2017-03-14 18:04:01 +0400

    ADMIN PANEL !

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,49 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Traits\FileUploadTrait;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Http\Request;
-use app\Regions;
-use DB;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
- function insert(Request $req){
-
-    	$regionName =$req->input('regionName');
-    	$regionCity = $req->input('regionCity');
-    	$aboutTours = $req->input('aboutTours');
-    	$aboutPlace =$req->input('aboutPlace');
-
-    	$data =array('regionName'=>$regionName,"regionCity"=>$regionCity, "aboutTours"=>$aboutTours,"aboutPlace"=>$aboutPlace);
-
-    	DB::table('regioninfo')->insert($data);
-
-    	echo "success";
-
-           
-    }
-
-
-
-
-    function getData()
-    {
-    	$data['data'] = DB::table('regionInfo')->get();
-    	if (count($data) > 0)
-    	{
-
-    		return view('insertForm', $data);
-    	}
-    	else
-    	{
-    		return view ('insertForm');
-    	}
-    }
-
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, FileUploadTrait;
 }
